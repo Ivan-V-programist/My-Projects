4dc23ebb3d81474392d29b9332252fdc3b062aa5
Ivan-V-programist <121342933+Ivan-V-programist@users.noreply.github.com>
2023-01-02 13:18:22 +0200

    Add files via upload

---
+++ text_generator.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras import layers
+from keras.layers import Dense, LSTM, Dropout
+import os
+import tqdm
+
+sequence_length =100
+# dataset file path
+FILE_PATH = "sample_data.txt"
+# FILE_PATH = "data/python_code.py"
+BASENAME = os.path.basename(FILE_PATH)
+
+seed = str(input("enter your seed:   "))
+
+# load vocab dictionaries
+char2int = pickle.load(open(f"{BASENAME}-char2int.pickle", "rb"))
+int2char = pickle.load(open(f"{BASENAME}-int2char.pickle", "rb"))
+vocab_size = len(char2int)
+
+# building the model
+model = Sequential([
+    LSTM(256, input_shape=(sequence_length, vocab_size), return_sequences=True),
+    Dropout(0.3),
+    LSTM(256),
+    Dense(vocab_size, activation="softmax"),
+])
+
+# load the optimal weights
+model.load_weights(f"results/{BASENAME}-{sequence_length}.h5")
+
+s = seed
+n_chars = 100
+# generate 400 characters
+generated = ""
+for i in tqdm.tqdm(range(n_chars), "Generating text"):
+    # make the input sequence
+    X = np.zeros((1, sequence_length, vocab_size))
+    for t, char in enumerate(seed):
+        X[0, (sequence_length - len(seed)) + t, char2int[char]] = 1
+    # predict the next character
+    predicted = model.predict(X, verbose=0)[0]
+    # converting the vector to an integer
+    next_index = np.argmax(predicted)
+    # converting the integer to a character
+    next_char = int2char[next_index]
+    # add the character to results
+    generated += next_char
+    # shift seed and the predicted character
+    seed = seed[1:] + next_char
+
+print("Seed:", s)
+print("Generated text:")
+print(generated)
